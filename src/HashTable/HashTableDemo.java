22938e851f7c09d6b56aed8d11720e2cf1d7d6cb
2985496686 <2985496686@qq.com>
2022-05-22 22:07:50 +0800

    JavaStruct

--- src/HashTable/HashTableDemo.java
+++ src/HashTable/HashTableDemo.java
@@ -1,50 +1,56 @@
 package HashTable;
 
-import java.util.Hashtable;
-import java.util.Objects;
-import java.util.Scanner;
-
 public class HashTableDemo {
 
-    //哈希表每个位置链表的节点
-    class Node{
-        int key;
-        String value;
-        Node next;
-        Node(int key, String value){
-            this.key = key;
-            this.value = value;
-            next = null;
-        }
-        public boolean equals(Node node){
-            if(this == node) return true;
-            else{
-                if(node == null) return false;
+        //哈希表每个位置链表的节点
+        class Node{
+            int key;
+            String value;
+            Node next;
+            Node(){
+            }
+            Node(int key, String value){
+                this.key = key;
+                this.value = value;
+                next = null;
+            }
+            public boolean equals(Node node){
+                if(this == node) return true;
                 else{
-                    return this.value == node.value && this.key == node.key;
+                    if(node == null) return false;
+                    else{
+                        return this.value == node.value && this.key == node.key;
+                    }
                 }
             }
         }
-    }
-    //哈希表的容量
-    int size;
-    //哈希表存的键值对个数
-    int count;
-    //加载因子
-    static double loadFactor = 0.75;
-    //存储数据容器
-    Node table[];
-    public HashTableDemo(){
-        size = 16;
-        count = 0;
-        table = new Node[size];
-        for (int i = 0; i < size; i++) {
-            table[i] = new Node(i,null);
+        //哈希表的长度
+        int length;
+        //哈希表存的键值对个数
+        int size;
+        //加载因子
+        static double loadFactor = 0.75;
+        //存储数据容器
+        Node table[];
+        public HashTableDemo(){
+            length = 16;
+            size = 0;
+            table = new Node[length];
+            for (int i = 0; i < length; i++) {
+                table[i] = new Node(i,null);
+            }
         }
-    }
+        public HashTableDemo(int length){
+            this.length = length;
+            size = 0;
+            table = new Node[length];
+            for (int i = 0; i < length; i++) {
+                table[i] = new Node(i,null);
+            }
+        }
     //哈希函数
     public int hashFunc(int key){
-        return key % size;
+        return key % length;
     }
 
 
@@ -58,16 +64,19 @@ public class HashTableDemo {
      */
     public String put(int key, String value){
         int index = hashFunc(key);
-            Node cur = table[index];
-            while(cur.next != null){
-                if(cur.next.key == key){
-                    cur.next.value = value;
-                    break;
+            //保证cur2始终是cur的前一个节点。
+            Node cur = table[index].next;
+            Node cur2 = table[index];
+            while(cur != null){
+                if(cur.key == key){
+                    cur.value = value;
+                    return value;
                 }
                 cur = cur.next;
+                cur2 = cur2.next;
             }
-            cur.next = new Node(key, value);
-            count++;
+            cur2.next = new Node(key, value);
+            size++;
         return value;
     }
 
@@ -83,7 +92,7 @@ public class HashTableDemo {
         Node cur = table[index];
         while(cur.next != null){
             if(cur.next.key == key){
-                count--;
+                size--;
                 String value = cur.next.value;
                 cur.next = cur.next.next;
                 return value;
@@ -95,7 +104,7 @@ public class HashTableDemo {
 
     //判断哈希表是否为空
     public boolean isEmpty(){
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < length; i++){
             if(table[i].next != null)
                 return false;
         }
@@ -104,7 +113,7 @@ public class HashTableDemo {
 
     //返回哈希表已存数据个数
     public int size(){
-        return count;
+        return size;
     }
 
     //通过key获取value
@@ -124,14 +133,15 @@ public class HashTableDemo {
     }
     //遍历哈希表
     public void print(){
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < length; i++){
             Node cur = table[i];
             System.out.printf("第%d条链表: ",i);
             if(cur.next == null){
                 System.out.println("null");
                 continue;
             }
-            while(cur.next != null){
+            cur = cur.next;
+            while(cur != null){
                 System.out.print(cur.key + "---"+ cur.value + "  ");
                 cur = cur.next;
             }
@@ -140,6 +150,22 @@ public class HashTableDemo {
     }
 
     public static void main(String[] args) {
-        System.out.println("ssssss");
+        HashTableDemo h = new HashTableDemo();
+        h.put(12,"张三");
+        h.put(14,"李四");
+        h.put(124,"张三");
+        h.put(122,"张三");
+        h.put(1455,"李四");
+        h.put(1224,"张三");
+        h.put(143,"李四");
+        h.put(5,"kkk");
+        h.put(21,"2222222");
+        h.put(1455,"李四");
+        h.put(1455,"李ffffff");
+        h.print();
+        h.remove(1455);
+        h.remove(5);
+        h.print();
+        System.out.println(h.size());
     }
 }
