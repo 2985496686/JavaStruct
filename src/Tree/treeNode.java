346291851092de177ec1f2a10423074f54577982
2985496686 <2985496686@qq.com>
2022-05-23 22:39:22 +0800

    JavaStruct

---
+++ src/Tree/treeNode.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class treeNode {
+        String data;
+        treeNode left;
+        treeNode right;
+        public treeNode(){}
+        public treeNode(String data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+}
